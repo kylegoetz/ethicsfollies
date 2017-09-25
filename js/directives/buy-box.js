45d249e2c37f0e0f2a97a65407678e2033a72ca2
Kyle Goetz <kylegoetz@gmail.com>
2017-09-25 00:03:41 -0500

    forgot a directive and JSON data in last commit

---
+++ js/directives/buy-box.js
@@ -0,0 +1,25 @@
+(function(){
+  angular
+    .module('folliesApp')
+    .directive('buyBox', BuyBox);
+
+  BuyBox.$inject = [];
+
+  function BuyBox() {
+    return {
+		link: function(scope) {
+			console.log('linking function run', scope);
+		},
+      restrict: 'E',
+      replace: true,
+      template: '<div class="show-panel"><h3 ng-bind="date"/><h4 ng-bind="time"/><p ng-if="isAwardNight">With <em>Lee Cusenbary Ethical Life Awards</em> and a catered, open bar networking reception.</p>CLE fee: <span ng-bind="cleFee"/> <a class="btn btn-default btn-sm" href="{{url}}">Buy</a></div>',
+      scope: {
+        date: '=',
+		time: '=',
+		isAwardNight: '=',
+		cleFee: '=',
+		url: '='
+      }
+    };
+  }
+})();
