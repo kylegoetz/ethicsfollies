214608081196f09dec52647a26089404f93c25c8
Kyle Goetz <kylegoetz@gmail.com>
2018-06-08 11:52:46 -0500

    update 2018 schedule

--- js/controllers/schedule/schedule.js
+++ js/controllers/schedule/schedule.js
@@ -5,36 +5,18 @@
 
   function schedule($scope, $rootScope, currentShow) {
     $scope.performances = [
-		{
-			date: 'May 5 (12pm)',
-			event: 'Abridged preview',
-			location: 'Central Jury Room, Cadena-Reeves Justice Center, San Antonio, Texas',
-			notes: 'SABA CLE'
-		},
-		{
-			date: 'June 2 (10am)',
-			event: 'Abridged preview',
-			location: 'Westin Riverwalk (Navarro Ballroom), San Antonio',
-			notes: '12th Anniversary SBOT Bench/Bar Bankruptcy Conference'
-		},
       {
-        date: 'October 25 (3:30pm)',
-        event: 'Being Walter Decent',
+        date: 'October 31 (2pm)',
+        event: currentShow.title,
         location: 'The Empire Theater, San Antonio, Texas',
         notes: 'Full performance'
       },
       {
-        date: 'October 26 (7pm)',
-        event: 'Being Walter Decent',
+        date: 'Nov 1 (7pm)',
+        event: currentShow.title,
         location: 'The Empire Theater, San Antonio, Texas',
         notes: 'Full performance'
-	},
-	{
-		date: 'November 10',
-		event: 'Being Walter Decent',
-		location: 'Morristown, New Jersey',
-		notes: ''
-	}
+      }
     ];
   }
 })();
