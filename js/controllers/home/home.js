dc30df4b0dd9e31e5ab4bd04303c76d75fd5ee72
Kyle Goetz <kylegoetz@gmail.com>
2018-06-08 11:52:32 -0500

    refactor Home

--- js/controllers/home/home.js
+++ js/controllers/home/home.js
@@ -2,9 +2,7 @@
   folliesControllers
     .controller('HomeCtrl', HomeCtrl);
 
-  HomeCtrl.$inject = ['$scope', '$rootScope', 'apiService'];
-
-  function HomeCtrl($scope,$rootScope, api){
+  function HomeCtrl($scope,$rootScope, apiService, currentShow){
 
     activate();
 
@@ -13,9 +11,10 @@
 
 
     function activate(){
-		api.getRegistrationInfo().then(function(data){
-			$scope.registration = data;
-		});
+      $scope.currentShow = currentShow;
+  		apiService.getRegistrationInfo().then(function(data){
+  			$scope.registration = data;
+  		});
       $scope.clickablePhone = $scope.contact.phoneNumber.replace('-','').replace('(','').replace(')','');
     }
   }
