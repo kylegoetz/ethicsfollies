35a2f58ecf262070d926758ffc078955cb0919d9
Kyle Goetz <kylegoetz@gmail.com>
2018-10-11 22:23:12 -0500

    refactor schedule

--- js/controllers/schedule/schedule.js
+++ js/controllers/schedule/schedule.js
@@ -3,20 +3,10 @@
     .module('folliesApp')
     .controller('ScheduleCtrl', schedule);
 
-  function schedule($scope, $rootScope, currentShow) {
-    $scope.performances = [
-      {
-        date: 'October 31 (2pm)',
-        event: currentShow.title,
-        location: 'The Empire Theater, San Antonio, Texas',
-        notes: 'Full performance'
-      },
-      {
-        date: 'Nov 1 (7pm)',
-        event: currentShow.title,
-        location: 'The Empire Theater, San Antonio, Texas',
-        notes: 'Full performance'
-      }
-    ];
+  function schedule($scope, $http, currentShow) {
+    $http.get('/resources/schedule.json')
+    .then(schedule => {
+      $scope.performances = schedule.data.map(event => Object.assign({event: currentShow.title}, event));
+    });
   }
 })();
