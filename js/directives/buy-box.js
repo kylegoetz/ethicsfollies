ec3fbb1664d622954f3c66ce263db742b87ed6d0
Kyle Goetz <kyle.goetz@stratiumconsulting.com>
2022-09-25 21:44:35 -0500

    improve code readability

--- js/directives/buy-box.js
+++ js/directives/buy-box.js
@@ -12,7 +12,15 @@
 		},
       restrict: 'E',
       replace: true,
-      template: '<div class="show-panel"><h3 ng-bind="date"/><h4 ng-bind="time"/><p ng-if="isAwardNight">With <em>Lee Cusenbary Ethical Life Awards</em> and a catered, open bar networking reception.</p>CLE fee: $<span ng-bind="cleFee"/> <a class="btn btn-default btn-sm" href="{{url}}">Register</a></div>',
+      template: `
+            <div class="show-panel">
+                <h3 ng-bind="date"/>
+                <h4 ng-bind="time"/>
+                <p ng-if="isAwardNight">
+                    With <em>Lee Cusenbary Ethical Life Awards</em> and a catered, open bar networking reception.
+                </p>
+                CLE fee: $<span ng-bind="cleFee"/> <a class="btn btn-default btn-sm" href="{{url}}">Register</a>
+            </div>`,
       scope: {
         date: '=',
 		time: '=',
